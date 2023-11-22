ec1a29a2f0f65577114a110519e1a09c7ac49b44
Yakiyo <yakiyo1206@gmail.com>
2023-11-22 12:59:37 +0000

    feat: init next project

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <p>
+        Personal api for using in my projects and stuff. Repository:
+        <a href="https://github.com/Yakiyo/api">Github</a>
+      </p>
+    </>
+  );
+}
